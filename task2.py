9651f1dc54c257862826ee3ef97d3c27e6612741
marychka2335 <marychka2335@icloud.com>
2024-10-27 15:00:31 +0200

    tasks commit

---
+++ task2.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+def is_palindrome(text):
+    """
+    Перевіряє, чи є рядок паліндромом.
+
+    Args:
+        text: Рядок, який потрібно перевірити.
+
+    Returns:
+        True, якщо рядок є паліндромом, False інакше.
+    """
+    text = ''.join(filter(str.isalnum, text.lower().replace(" ", "")))
+    deque_text = deque(text)
+    
+    while len(deque_text) > 1:
+        if deque_text.popleft() != deque_text.pop():
+            return False
+            
+    return True
+
+# Перевірка 
+print(is_palindrome("Racecar")) # True
+print(is_palindrome("Madam, I'm Adam")) # True
+print(is_palindrome("A Toyota's a Toyota")) # True
+print(is_palindrome("Hello world")) # False
